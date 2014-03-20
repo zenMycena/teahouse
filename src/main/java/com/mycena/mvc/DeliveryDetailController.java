d5b156b76523cc002c4bd37b85b475bb30b1e0ca
zen <zen.mycena@gmail.com>
2014-03-20 18:26:32 +0800

    fixed html CRUD

--- src/main/java/com/mycena/mvc/DeliveryDetailController.java
+++ src/main/java/com/mycena/mvc/DeliveryDetailController.java
@@ -7,20 +7,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.mycena.data.User;
+
 @Controller
-@RequestMapping("/myOrder")
-public class MyOrderController {
-	
+@RequestMapping("/deliveryDetail")
+public class DeliveryDetailController {
 	@Autowired
 	private Basket basket;
+	private User user;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String myOrderPage() {
-		return "menuitems/myOrder";
+		return "menuitems/deliveryDetail";
 	}
 	
 	@ModelAttribute("basket")
 	private Basket getBasket() {
 		return basket;
+	}
+	
+	@ModelAttribute("user")
+	private User getUser() {
+		return user;
 	}
 }
