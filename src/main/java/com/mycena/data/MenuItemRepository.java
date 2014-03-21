25358e14693ab384268f6abe61e9f9c0bbb6ffb1
Jade <jade.mycena@gmail.com>
2014-03-21 21:46:02 +0800

    MenuItemController & MenuItemRepository Fixed.

--- src/main/java/com/mycena/data/MenuItemRepository.java
+++ src/main/java/com/mycena/data/MenuItemRepository.java
@@ -9,4 +9,6 @@ public interface MenuItemRepository extends CrudRepository<MenuItem, Long>{
 	
 	@Query("from Message where id > :id")
     List<MenuItem> findWithMaxOrder(long id);
+	
+	MenuItem findByName(String name); 
 }
